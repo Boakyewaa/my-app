7a59d32e80fc70fab77f2a8a84367bd320d7dfc2
Berlie <berl.yiadom@gmail.com>
2022-11-04 20:30:14 +0000

    Included the navigation Bar

---
+++ src/NaviBar.js
@@ -0,0 +1,22 @@
+
+import './NaviBar.css';
+
+function Navibar() {
+    return(
+        <div className='Nav-Bar'>
+            <ul>
+                <li>
+                    <a href=''>About</a>
+                </li>
+                <li>
+                    <a href=''>Ministries</a>
+                </li>
+                <li>
+                    <a href=''>Resources</a>
+                </li>
+            </ul>
+
+        </div>
+    );
+}
+export default Navibar;
