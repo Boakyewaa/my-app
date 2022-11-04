80e32b1577e517951e761731479462a827580015
Berlie <berl.yiadom@gmail.com>
2022-11-04 20:47:06 +0000

    Edited the content in App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>PETROLEUM ESTATE WORSHIP CENTER</h1>
-        <h3>You're welcome to service with us</h3>
+        <h1>Welcome to our Church</h1>
+        <h3>Petroleum Estate Worship Center</h3>
       </header>
       
     </div>
